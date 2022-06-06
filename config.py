e6996cf402c9653b9a6882c877309b52d76f25e2
diago8888 <102332780+diago8888@users.noreply.github.com>
2022-06-06 11:24:25 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 BOT_TOKEN = os.environ.get('bot_token')
-API_ID = 12729863
+API_ID = os.environ.get(api_id)
 API_HASH = os.environ.get('api_hash') 
 
 
 # DATOS DEL MOODLE
 USUARIO = os.environ.get('user')
 PASSWORD = os.environ.get('password')
-USUARIO_ID = 11609
-ZIP_MB = 399
+USUARIO_ID = os.environ.get(user_id)
+ZIP_MB = os.environ.get(zips_size)
 MOODLE_URL = os.environ.get('host')
-OWNER = [1906940331]
+OWNER = os.environ.get([owner])
 
 # CUENTA DE MEGA
 GMAIL_MEGA = 'diago8888diago@gmail.com'
