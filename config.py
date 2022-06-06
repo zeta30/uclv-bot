1c7e5ef784e52044457cd0aaf54c28458edf95d1
diago8888 <102332780+diago8888@users.noreply.github.com>
2022-06-06 12:04:49 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 BOT_TOKEN = os.environ.get('bot_token')
-API_ID = os.environ.get(api_id)
+API_ID = os.environ.get('api_id')
 API_HASH = os.environ.get('api_hash') 
 
 
 # DATOS DEL MOODLE
 USUARIO = os.environ.get('user')
 PASSWORD = os.environ.get('password')
-USUARIO_ID = os.environ.get(user_id)
-ZIP_MB = os.environ.get(zips_size)
+USUARIO_ID = os.environ.get('user_id')
+ZIP_MB = os.environ.get('zips_size')
 MOODLE_URL = os.environ.get('host')
 OWNER = [1906940331]
 
